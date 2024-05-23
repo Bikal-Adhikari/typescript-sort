5e773f6d63a9ca2357e4597e406d8f71178f8349
Bikal <bikaladhikari5@gmail.com>
2024-05-23 19:30:39 +1000

    separating swapping and comparision

--- build/Sorter.js
+++ build/Sorter.js
@@ -2,32 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 class Sorter {
-    constructor(collection /*Todo fix me*/) {
+    constructor(collection) {
         this.collection = collection;
     }
     sort() {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                //All of this only works if collection is number []
-                //if collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                //All of this only works if collection is a string
-                // if collection is a string, do this logic instead:
-                //----logic to compare and swap characters in a string
-                if (typeof this.collection === "string") {
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
                 }
             }
         }
     }
 }
 exports.Sorter = Sorter;
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
