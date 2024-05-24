2098e2eed95b611c4a078f8660e2a793a898a40c
Bikal <bikaladhikari5@gmail.com>
2024-05-24 17:36:24 +1000

    using abstract classes use of instead of interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { Sorter } from "./Sorter";
+
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([100, -3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
-const sorterChars = new Sorter(charactersCollection);
-sorterChars.sort();
+// const sorterChars = new Sorter(charactersCollection);
+// sorterChars.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -19,6 +21,6 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorterList = new Sorter(linkedList);
-sorterList.sort();
+// const sorterList = new Sorter(linkedList);
+linkedList.sort();
 linkedList.print();
